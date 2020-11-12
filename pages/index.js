423c7e9b40b65c41453516d6ec2c0fac0ae1a878
Paprika-a11y <1303678462@qq.com>
2020-11-12 15:21:49 +0800

    changed

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const MainPage = props => {
   const remDonut = () => setNumDonuts(num => Math.max(1, num - 1));
 
   return (
-    <Layout title="Donut Shop">
+    <Layout title="MyDonut Shop">
       <Row>
         <DonutShop
           onAddDonut={addDonut}
